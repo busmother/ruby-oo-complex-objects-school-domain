f253512083e3a6841b2004414c2e01aa854f62e1
Ian Marshall <imarshall14@gmail.com>
2020-12-16 15:14:43 -0500

    commit tree 2584dff91886618646c19f51f0860c6fdfa5f403

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
 # code here!
+require "pry"
+
+class School
+    attr_accessor :roster
+    def initialize(school)
+        @school = school
+        @roster = {}
+    end
+    def school
+        @school
+    end
+    def add_student(student_name, grade)
+        if roster[grade]
+            roster[grade] << student_name
+        else 
+            roster[grade] = []
+            roster[grade] << student_name
+        end
+    end
+    def grade(num)
+        roster[num]
+    end
+    def sort
+        new_hash = {}
+        roster.sort.map do |grade, student|
+            new_hash[grade] = student.sort
+        end
+        new_hash
+    end
+end
